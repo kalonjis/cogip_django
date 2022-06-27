33874fa52e6450cad763b209f5244989c2e795f1
kalonjis <kalonjistephane@gmail.com>
2022-06-27 15:49:11 +0200

    create countries list file + add country col for company model

--- office/models.py
+++ office/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from office.countries import COUNTRIES
+
 """Company
     -name,
     -num tva,
@@ -19,3 +21,8 @@ class Company(models.Model):
     slug = models.SlugField()
     vat_number = models.CharField(max_length=100, blank=True)
     type = models.CharField(max_length=100, choices=CHOICES, default='Prov')
+    country = models.CharField(max_length=100, choices=COUNTRIES, blank=True)
+
+    def __str__(self):
+        return self.name
+
