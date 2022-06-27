b0467acedf1ad4bc750700ddddb07ff64f872050
kalonjis <kalonjistephane@gmail.com>
2022-06-27 16:09:27 +0200

    add method to define slug automatically

--- office/models.py
+++ office/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 from office.countries import COUNTRIES
 
@@ -18,11 +19,19 @@ class Company(models.Model):
         ('Cli', 'Client')
     )
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=120, unique=True, blank=True)
     vat_number = models.CharField(max_length=100, blank=True)
     type = models.CharField(max_length=100, choices=CHOICES, default='Prov')
     country = models.CharField(max_length=100, choices=COUNTRIES, blank=True)
 
+    class Meta:
+        verbose_name = "Compagnie"
+
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
