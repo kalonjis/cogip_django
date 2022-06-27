801f9d1e875f8ee35c817f57fc426206f02036bf
kalonjis <kalonjistephane@gmail.com>
2022-06-27 14:34:21 +0200

    model company init (name, slug, vat_number, type)

--- office/models.py
+++ office/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+"""Company
+    -name,
+    -num tva,
+    -type (provider or client),
+    -invoices,
+    -contacts
+    """
+
+
+class Company(models.Model):
+    # Type of company choices
+    CHOICES = (
+        ('Prov', 'Provider'),
+        ('Cli', 'Client')
+    )
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    vat_number = models.CharField(max_length=100, blank=True)
+    type = models.CharField(max_length=100, choices=CHOICES, default='Prov')
