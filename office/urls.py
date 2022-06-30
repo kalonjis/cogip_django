672a8eb290531dc312eb43595608c6eed3e7ac53
kalonjis <kalonjistephane@gmail.com>
2022-06-30 18:46:38 +0200

    changed 'context_object_name' and added 'name' attribute for contact/company/invoice

--- office/urls.py
+++ office/urls.py
@@ -5,10 +5,8 @@ from office.views import HomeView
 
 
 urlpatterns = [
-    path('company/', HomeView.as_view(model=Company, template_name='office/company_home.html', context_object_name='company-home')),
-    path('contact/',
-         HomeView.as_view(model=Contact, template_name='office/contact_home.html', context_object_name='contact-home')),
-    path('invoice/',
-         HomeView.as_view(model=Invoice, template_name='office/invoice_home.html', context_object_name='invoice-home')),
+    path('company/', HomeView.as_view(model=Company, template_name='office/company_home.html', context_object_name='companies'), name='company-home'),
+    path('contact/', HomeView.as_view(model=Contact, template_name='office/contact_home.html', context_object_name='contacts'), name='contact-home'),
+    path('invoice/', HomeView.as_view(model=Invoice, template_name='office/invoice_home.html', context_object_name='invoices'), name='invoice-home'),
 
 ]
