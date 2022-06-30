b83bad2415e5f4dd477d4c38cb7d5603a3ffaa9a
kalonjis <kalonjistephane@gmail.com>
2022-06-30 18:41:29 +0200

    defined class HomeView(ListView), urls and home.html for contact/company/invoice

--- office/urls.py
+++ office/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
 
+from office.models import Company, Contact, Invoice
+from office.views import HomeView
+
 
 urlpatterns = [
+    path('company/', HomeView.as_view(model=Company, template_name='office/company_home.html', context_object_name='company-home')),
+    path('contact/',
+         HomeView.as_view(model=Contact, template_name='office/contact_home.html', context_object_name='contact-home')),
+    path('invoice/',
+         HomeView.as_view(model=Invoice, template_name='office/invoice_home.html', context_object_name='invoice-home')),
 
 ]
--- office/views.py
+++ office/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from office.models import Company
 from django.views import View
 
 
-class HomeView(View):
-    def get(self, request):
-        return render(request, 'office/home.html')
-
+class HomeView(ListView):
+    model = 'default'
+    template_name = 'office/home.html'
+    context_object_name = 'default'
 
