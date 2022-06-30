7006039118d5b65baaf86fffe54663425485c5fa
kalonjis <kalonjistephane@gmail.com>
2022-06-30 13:41:54 +0200

    created views.index

--- office/urls.py
+++ office/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 
-from office.views import  base
 
 urlpatterns = [
-    path('', base, name='base')
+
 ]
--- office/views.py
+++ office/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from office.models import Company
 
 
-def base(request):
-    return render(request, 'office/base.html')
+def index(request):
+    return render(request, 'office/index.html')
