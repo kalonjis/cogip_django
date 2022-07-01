5b2a67ea450cf76be78c0f4f06e8b5339c5ee72d
kalonjis <kalonjistephane@gmail.com>
2022-07-01 17:36:08 +0200

    refactor url for contact/invoice_home after restructring

--- office/urls.py
+++ office/urls.py
@@ -5,8 +5,8 @@ from office.views import HomeView, OfficeDetailView
 
 urlpatterns = [
     path('company/', HomeView.as_view(model=Company, template_name='office/company/company_home.html', context_object_name='companies'), name='company-home'),
-    path('contact/', HomeView.as_view(model=Contact, template_name='office/contact_home.html', context_object_name='contacts'), name='contact-home'),
-    path('invoice/', HomeView.as_view(model=Invoice, template_name='office/invoice_home.html', context_object_name='invoices'), name='invoice-home'),
+    path('contact/', HomeView.as_view(model=Contact, template_name='office/contact/contact_home.html', context_object_name='contacts'), name='contact-home'),
+    path('invoice/', HomeView.as_view(model=Invoice, template_name='office/invoice/invoice_home.html', context_object_name='invoices'), name='invoice-home'),
     path('company/<str:slug>/',
          OfficeDetailView.as_view(model=Company, template_name='office/company/company_detail.html', context_object_name='company'),
          name='company-detail'),
