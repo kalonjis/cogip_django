146b3376e9dc90e79db1f8ea08bf4cd609647aaa
kalonjis <kalonjistephane@gmail.com>
2022-07-13 20:28:35 +0200

    forms.py file creation with UserRegistrationForm

---
+++ office/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from office.models import CustomUser
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username")
