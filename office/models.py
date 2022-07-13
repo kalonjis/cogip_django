6fa359049f8f2954c163d868b962de733ae74489
kalonjis <kalonjistephane@gmail.com>
2022-07-13 20:14:59 +0200

    CustomUser + CustomUserManager defined in models and AUTH_USER_MODEL in settings

--- office/models.py
+++ office/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import apps
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
@@ -84,3 +86,29 @@ class Invoice(models.Model):
     def get_absolute_url(self):
         return reverse('office:invoice-detail', kwargs={'pk': self.pk})
 
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password):
+        if not username:
+            raise ValueError("Vous devez rentrer un nom d'utilisateur")
+        user = self.model(username=username)
+        user.set.password(password)
+        user.save()
+        return user
+
+    def create_super_user(self, username, password):
+        user = self.create_user(username=username, password=password)
+        user.is_staff = True
+        user.is_admin = True
+        user.save()
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    username = models.CharField(max_length=100, unique=True, blank=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "username"
+    objects = CustomUserManager()
